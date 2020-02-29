b9ae6f973463ac637b117c67d40ff01af21a0b64
abraartishan <abraartishan@localhost>
2020-02-29 18:22:38 -0500

    2nd Commit

---
+++ Test Suites/topa.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>topa</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>516bb944-616f-40c2-b844-ddf4a7d3a604</testSuiteGuid>
+</TestSuiteEntity>
