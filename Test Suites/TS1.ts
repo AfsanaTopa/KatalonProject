602d710c39b0596c5205a9360682ff5625576f9c
abraartishan <abraartishan@localhost>
2020-02-29 19:44:43 -0500

    TEST SUITE NAME CHANGE

--- Test Suites/TS1.ts
+++ Test Suites/TS1.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>topa</name>
+   <name>TS1</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,4 +10,10 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>516bb944-616f-40c2-b844-ddf4a7d3a604</testSuiteGuid>
+   <testCaseLink>
+      <guid>2910cacc-d800-4f89-a271-8c21750a7e62</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc1</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
